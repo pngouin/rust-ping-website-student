b91a4ea5fac8a9b55bfba4695db663e46d7f258f
Hugo Zammit <hugo.zammit@signature-code.com>
2020-01-17 18:50:26 +0100

    Add ping JSON endpoint in server.

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,24 +7,34 @@
 extern crate reqwest;
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
 mod ping_utility;
 
 use crate::ping_utility::ping_domain;
 use rocket::request::Form;
+use rocket_contrib::json::Json;
 
 fn main() {
     rocket::ignite().mount("/", routes![ping]).launch();
 }
 
 #[derive(FromForm)]
-pub struct PingTask {
+struct PingRequest {
     domain: String,
     iterations: i64,
 }
 
+#[derive(Serialize)]
+struct PingResponse {
+    ping: i64,
+}
+
 #[post("/ping", data = "<task>")]
-fn ping(task: Form<PingTask>) -> String {
+fn ping(task: Form<PingRequest>) -> Json<PingResponse> {
     let result = ping_domain(task.domain.as_str(), task.iterations);
-    return result.to_string();
+    let response = Json(PingResponse { ping: result });
+    return response;
 }

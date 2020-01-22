72e6dc847a3c46ab53fc6eb36c9af53b42226751
Hugo Zammit <hugo.zammit@signature-code.com>
2020-01-22 19:28:46 +0100

    [FIX] Add cors in server.
    Thanks @ZooPin for finding a related blog post.

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,15 +7,46 @@
 extern crate reqwest;
 #[macro_use]
 extern crate rocket;
+extern crate rocket_cors;
 #[macro_use]
 extern crate serde_derive;
 
 mod ping;
 
-fn rocket() -> rocket::Rocket {
-    return rocket::ignite().mount("/", routes![crate::ping::ping_endpoint]);
+use rocket::http::Method;
+use rocket_cors::{ AllowedHeaders, AllowedOrigins, Error, Cors, CorsOptions };
+
+fn cors() -> Cors {
+    let allowed_origins = AllowedOrigins::some_exact(&[
+        "http://localhost:8080"
+    ]);
+
+    return CorsOptions {
+        allowed_origins,
+        allowed_methods:vec![
+            Method::Get,
+            Method::Post
+        ].into_iter().map(From::from).collect(),
+        allowed_headers:AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin"
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS");
 }
 
-fn main() {
+fn rocket() -> rocket::Rocket {
+    return rocket::ignite()
+        .mount("/", routes![crate::ping::ping_endpoint])
+        .attach(cors());
+}
+
+fn main() -> Result<(), Error> {
     rocket().launch();
+
+    return Ok(());
 }
